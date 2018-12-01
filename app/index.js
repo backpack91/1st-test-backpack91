9a7bfe3d880984fb74956bdf8034ec2590b5e7b9
backpack91 <bingrae1122@gmail.com>
2018-12-02 00:23:51 +0900

    modify initial style and add printed words style

--- app/index.js
+++ app/index.js
@@ -7,23 +7,25 @@ import 'styles/index.less';
 
 var DataMiner = {
   countedWords : {},
-  input : document.querySelector('.textInput')
+  input : document.querySelector('.textInput'),
+  chart : document.querySelector('.dataChart'),
+  wordsWrapper : document.querySelector('.wordsWrapper'),
+  colorData : []
 };
 
 DataMiner.makeInputReact = function() {
-  this.input.addEventListener('keypress', function(event) {
+  this.input.addEventListener('keydown', function(event) {
     DataMiner.judgeDatas();
   })
 }
 
 DataMiner.judgeDatas = function() {
-  var stringData = this.input.value
+  var stringData = this.input.value;
   var characters = stringData.split('');
   var words = stringData.split(' ');
-  console.log(characters);
-  console.log(words);
 
   if ( characters.length <= 5000 ) {
+    this.wordsWrapper.innerText = '';
     this.filterWords(words);
   } else {
     alert('please write under 5000 characters');
@@ -41,16 +43,41 @@ DataMiner.filterWords = function(words) {
       source.push(word);
     }
   });
-
   this.countWords(source);
 };
 
 DataMiner.countWords = function(source) {
-  
+  var countWords = {};
+
+  source.forEach(function(word) {
+    if( !countWords[word] ) {
+      countWords[word] = 1;
+    } else {
+      countWords[word] += 1;
+    }
+  });
+
+  this.printDatas(countWords);
 };
 
-DataMiner.printDatas = function(obj) {
-
+DataMiner.printDatas = function(countWords) {
+  for( var key in countWords ) {
+    var newWord = document.createElement('div');
+    var randomColor = this.makeRandomColor();
+    newWord.innerText = key;
+    newWord.style = `font-size: ${countWords[key]*15}px; color: ${randomColor}`;
+    newWord.classList.add('words');
+    this.wordsWrapper.appendChild(newWord);
+  }
 };
+
+DataMiner.makeRandomColor = function() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 DataMiner.makeInputReact();
