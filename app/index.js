b659eb8577595134c6e3690e54d4a66c5bc0b9b0
backpack91 <bingrae1122@gmail.com>
2018-12-01 21:22:26 +0900

    build basic structure
    make DataMiner object and functioins

--- app/index.js
+++ app/index.js
@@ -4,3 +4,53 @@ import 'styles/index.less';
 // ================================
 // START YOUR APP HERE
 // ================================
+
+var DataMiner = {
+  countedWords : {},
+  input : document.querySelector('.textInput')
+};
+
+DataMiner.makeInputReact = function() {
+  this.input.addEventListener('keypress', function(event) {
+    DataMiner.judgeDatas();
+  })
+}
+
+DataMiner.judgeDatas = function() {
+  var stringData = this.input.value
+  var characters = stringData.split('');
+  var words = stringData.split(' ');
+  console.log(characters);
+  console.log(words);
+
+  if ( characters.length <= 5000 ) {
+    this.filterWords(words);
+  } else {
+    alert('please write under 5000 characters');
+  }
+};
+
+DataMiner.filterWords = function(words) {
+  var source = [];
+
+  words.forEach(function(word) {
+    if( word !== 'am' &&
+    word !== 'are' &&
+    word !== 'were' &&
+    word !== 'was') {
+      source.push(word);
+    }
+  });
+
+  this.countWords(source);
+};
+
+DataMiner.countWords = function(source) {
+  
+};
+
+DataMiner.printDatas = function(obj) {
+
+};
+
+DataMiner.makeInputReact();
