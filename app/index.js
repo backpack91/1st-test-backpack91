b7a68b6ee357296a497ec4594f04603888d3bd17
backpack91 <bingrae1122@gmail.com>
2018-12-04 06:13:40 +0900

    add some eventHandler
    -let users know about frequency of usage of hovered word
    -make function to move word that  is covering another word which user wants to check out

--- app/index.js
+++ app/index.js
@@ -4,16 +4,21 @@ import 'styles/index.less';
 // ================================
 // START YOUR APP HERE
 // ================================
+var styleInfo = {};
 
 function DataMiner() {
   this.input = document.querySelector('.textInput');
   this.chart = document.querySelector('.dataChart');
   this.wordsWrapper = document.querySelector('.wordsWrapper');
-  this.singleUsedWords = document.querySelector('.singleUsedWords');
+  this.singleUsedWordsWrapper = document.querySelector('.singleUsedWordsWrapper');
+  this.leftRandomLocatingChart = document.querySelector('.leftRandomLocatingChart');
+  this.rightRandomLocatingChart = document.querySelector('.rightRandomLocatingChart');
+  this.upperRandomLocatingChart = document.querySelector('.upperRandomLocatingChart');
+  this.lowerRandomLocatingChart = document.querySelector('.lowerRandomLocatingChart');
+  this.oftenUsedWordsWrapper = document.querySelector('.oftenUsedWordsWrapper');
 }
 
 DataMiner.prototype.makeInputReact = function() {
-
   this.input.addEventListener('keydown', function() {
     this.judgeDatas();
   }.bind(this));
@@ -29,12 +34,21 @@ DataMiner.prototype.judgeDatas = function() {
   if (document.querySelector('.alertMessage')) {
     document.querySelector('.alertMessage').remove();
     this.wordsWrapper.classList.remove('hide');
+    this.singleUsedWordsWrapper.classList.remove('hide');
+    this.oftenUsedWordsWrapper.classList.remove('hide');
   }
   if (characters.length <= 5000) {
     this.wordsWrapper.innerText = '';
+    this.singleUsedWordsWrapper.innerText = '';
+    this.leftRandomLocatingChart.innerText = '';
+    this.rightRandomLocatingChart.innerText = '';
+    this.upperRandomLocatingChart.innerText = '';
+    this.lowerRandomLocatingChart.innerText = '';
     this.filterWords(words);
   } else {
+    this.singleUsedWordsWrapper.classList.add('hide');
     this.wordsWrapper.classList.add('hide');
+    this.oftenUsedWordsWrapper.classList.add('hide');
     var alertMessage = document.createElement('div');
     alertMessage.classList.add('alertMessage');
     alertMessage.innerText= "please type under 5000...";
@@ -46,6 +60,8 @@ DataMiner.prototype.judgeDatas = function() {
     (12593 < character.charCodeAt(0) && character.charCodeAt(0) < 12622) ||
     (12623 < character.charCodeAt(0) && character.charCodeAt(0) < 12643)) {
       this.wordsWrapper.classList.add('hide');
+      this.singleUsedWordsWrapper.classList.add('hide');
+      this.oftenUsedWordsWrapper.classList.add('hide');
       var alertMessage = document.createElement('div');
       alertMessage.classList.add('alertMessage');
       alertMessage.innerText= "please type only ENGLISH";
@@ -104,12 +120,20 @@ DataMiner.prototype.filterWords = function(words) {
     lowerCasedWord !== '' &&
     lowerCasedWord !== 'for' &&
     lowerCasedWord !== 'that' &&
-    lowerCasedWord !== 'you\'ll' &&
+    lowerCasedWord !== 'on' &&
+    lowerCasedWord !== 'by' &&
+    lowerCasedWord !== 'an' &&
+    lowerCasedWord !== 'in' &&
+    lowerCasedWord !== 'or' &&
+    lowerCasedWord !== 'it' &&
+    lowerCasedWord !== 'but' &&
+    lowerCasedWord !== 'as' &&
+    lowerCasedWord !== 'if' &&
+    lowerCasedWord !== 'they' &&
+    lowerCasedWord !== 'we' &&
     lowerCasedWord !== 'for' &&
-    // lowerCasedWord !== 'for' &&
-    // lowerCasedWord !== 'for' &&
-    // lowerCasedWord !== 'for' &&
-
+    lowerCasedWord !== 'with' &&
+    lowerCasedWord !== 'could' &&
     lowerCasedWord !== 'was') {
       source.push(word);
     }
@@ -129,8 +153,12 @@ DataMiner.prototype.countWords = function(source) {
 DataMiner.prototype.printDatas = function(countedWords) {
   var oftenUsedWords = {};
   var wordsWrapper = this.wordsWrapper;
-  var top30Words = [];
-  var count30 = 0;
+  var randomChartCollection = [];
+  var singleWords = {};
+  randomChartCollection.push(this.leftRandomLocatingChart);
+  randomChartCollection.push(this.rightRandomLocatingChart);
+  randomChartCollection.push(this.upperRandomLocatingChart);
+  randomChartCollection.push(this.lowerRandomLocatingChart);
 
   for (var key in countedWords) {
     if (countedWords[key] >= 2) {
@@ -140,65 +168,113 @@ DataMiner.prototype.printDatas = function(countedWords) {
         oftenUsedWords[countedWords[key]] = [];
         oftenUsedWords[countedWords[key]].push(key);
       }
-      delete countedWords[key];
     }
+    singleWords[key] = countedWords[key];
   }
   console.log('oftenUsedWords: ', oftenUsedWords);
   console.log('countedWords: ', countedWords);
   console.log('keysOfOftenUsedWords: ', Object.keys(oftenUsedWords));
 
-  for (var key in countedWords) {
+  for (var key in singleWords) {
     var left = Math.random()*100;
     var top = Math.random()*100;
     var newWord = document.createElement('div');
-    newWord.classList.add('words');
+    newWord.classList.add('singleUsedWord');
     newWord.innerText = key;
-    newWord.style = `font-size: 15px; color: gray; left: ${left}%; top: ${top}%; z-index: 1;`
-    this.wordsWrapper.appendChild(newWord);
+    newWord.style = `font-size: 15px; color: #222222; left: ${left}%; top: ${top}%; z-index: 1;`
+    this.singleUsedWordsWrapper.appendChild(newWord);
   }
 
   var frequencyList = Object.keys(oftenUsedWords);
 
   for (var i = frequencyList.length-1; i >= 0; i--) {
     var thisFrequencyWords = oftenUsedWords[frequencyList[i]];
+    var frequency = frequencyList[i];
+    console.log('frequency: ', frequency);
 
     for (var j = 0; j < thisFrequencyWords.length; j++) {
       var left = Math.random()*100;
       var top = Math.random()*100;
       var newWord = document.createElement('div');
       var randomColor = this.makeRandomColor();
-      var fontSize = 100 * ( frequencyList[i]/ frequencyList[frequencyList.length-1]);
-      newWord.innerText =thisFrequencyWords[j];
+      var fontSize = 70 * ( frequencyList[i]/ frequencyList[frequencyList.length-1]);
+      var width;
+      var height;
+      var styleBefore;
+
+      newWord.innerText = thisFrequencyWords[j];
       newWord.classList.add('words');
       console.log(fontSize);
-      this.wordsWrapper.appendChild(newWord);
-      if ( i === frequencyList.length-1 && j === 0 ) {
+      if (!(i == frequencyList.length-1 && j == 0)) {
+        var randomIndex = Math.floor(Math.random()*4);
+        var chartToAppend = randomChartCollection[randomIndex];
+        console.log('randomChart: ', chartToAppend);
+        chartToAppend.appendChild(newWord);
+      } else {
+        this.wordsWrapper.appendChild(newWord);
+      }
+      if (i === frequencyList.length-1 && j === 0) {
+        width = newWord.offsetWidth;
+        height = newWord.offsetHeight;
         var newWordWidth = (newWord.offsetWidth / newWord.parentNode.offsetWidth) * 100;
-        var newWordHeight = (newWord.offsetHeight / newWord.parentNode.offsetHeight) * 100
+        var newWordHeight = (newWord.offsetHeight / newWord.parentNode.offsetHeight) * 100;
         newWord.classList.add('textShadowWhite');
-        newWord.style = `font-size: ${130}px; color: ${randomColor}; left: ${50 - (newWordWidth*3)}%; top: ${50 - (newWordHeight*3)}%; z-index: ${i+3};`;
+        newWord.style = `font-size: ${100}px; color: ${randomColor}; left: ${50 - (newWordWidth*3)}%; top: ${50 - (newWordHeight*3)}%; z-index: ${i+2};`;
+        newWord.addEventListener('mouseout', function(event) {
+          event.target.classList.remove('hoveredCenter');
+          event.target.style.zIndex = styleInfo.zIndex;
+          event.target.style.fontSize = styleInfo.fontSize;
+          event.target.children[0].remove();
+        });
+        newWord.addEventListener('mouseover', function(event,frequency) {
+          var textBox = document.createElement('div');
+          textBox.innerText = ` :${countedWords[event.target.innerText]}`;
+          styleInfo.zIndex = event.target.style.zIndex;
+          styleInfo.fontSize = event.target.style.fontSize;
+          event.target.style.zIndex = '';
+          // event.target.style.fontSize = '';
+          event.target.classList.add('hoveredCenter');
+          event.target.appendChild(textBox);
+        });
+        // newWord.addEventListener('click', function(event) {
+        //   function randomLocation () {
+        //     return Math.random()*100;
+        //   }
+        //   event.target.style.left = '';
+        //   event.target.style.top = '';
+        //   event.target.style.left = `${randomLocation()}%`;
+        //   event.target.style.top = `${randomLocation()}%`;
+        // });
       } else {
         newWord.classList.add('textShadowBlack');
-        newWord.style = `font-size: ${fontSize}px; color: ${randomColor}; left: ${left}%; top: ${top}%; z-index: ${i+2};`;
+        newWord.style = `font-size: ${fontSize}px; color: ${randomColor}; left: ${left}%; top: ${top}%; z-index: ${i+1};`;
+        newWord.addEventListener('mouseout', function(event) {
+          event.target.classList.remove('hovered');
+          event.target.style.zIndex = styleInfo.zIndex;
+          event.target.style.fontSize = styleInfo.fontSize;
+          event.target.children[0].remove();
+        });
+        newWord.addEventListener('mouseover', function(event) {
+          var textBox = document.createElement('div');
+          textBox.innerText = ` :${countedWords[event.target.innerText]}`;
+          styleInfo.zIndex = event.target.style.zIndex;
+          styleInfo.fontSize = event.target.style.fontSize;
+          event.target.style.zIndex = '';
+          event.target.style.fontSize = '';
+          event.target.classList.add('hovered');
+          event.target.appendChild(textBox);
+        })
+        newWord.addEventListener('click', function(event) {
+          function randomLocation () {
+            return Math.random()*100;
+          }
+          event.target.style.left = '';
+          event.target.style.top = '';
+          event.target.style.left = `${randomLocation()}%`;
+          event.target.style.top = `${randomLocation()}%`;
+        });
       }
     }
-
-
-
-
-    // if (keys[keys.length-1] && keys[keys.length-1] === key) {
-      // newWord.style = `font-size: ${oftenUsedWords[key]*15}px; color: ${randomColor}; left: ${left}%; top: ${top}%; z-index: 5;`
-    // }
-    // if (keys[keys.length-2] && keys[keys.length-2] === key) {
-    //   newWord.style = `font-size: ${oftenUsedWords[key]*15}px; color: ${randomColor}; left: ${left}%; top: ${top}%; z-index: 4;`
-    // }
-    // if (keys[keys.length-3] && keys[keys.length-3] === key) {
-    //   newWord.style = `font-size: ${oftenUsedWords[key]*15}px; color: ${randomColor}; left: ${left}%; top: ${top}%; z-index: 3;`
-    // }
-    // if (keys[keys.length-4] && keys[keys.length-4] === key) {
-    //   newWord.style = `font-size: ${oftenUsedWords[key]*15}px; color: ${randomColor}; left: ${left}%; top: ${top}%; z-index: 3;`
-    // }
-
   }
 };
 
@@ -214,32 +290,3 @@ DataMiner.prototype.makeRandomColor = function() {
 
 var dataMiner = new DataMiner();
 dataMiner.makeInputReact();
-
-// function locateRandom(word) {
-//   var left = Math.random()*100;
-//   var top = Math.random()*100;
-//   var width = word.offsetWidth / (wordsWrapper.offsetWidth) * 100;
-//   var height = word.offsetHeight / (wordsWrapper.offsetHeight) *100;
-//   var location = {};
-//
-//   for (var i = 0; i < occupiedList.length; i++) {
-//     console.log(occupiedList[i].start);
-//     console.log(occupiedList[i].end);
-//
-//     debugger;
-//     while ( !( left + width < occupiedList[i].start[0] ||
-//     occupiedList[i].end[0] < left ||
-//     top + height < occupiedList[i].start[1] ||
-//     occupiedList[i].end[1] < top ) ) {
-//       left = Math.random()*100;
-//       top = Math.random()*100;
-//     }
-//   }
-//   location.name =
-//   location.start = [left, top];
-//   location.end = [left + width, top + height];
-//   occupiedList.push(location);
-//   console.log('occupiedList: ', occupiedList, word);
-//
-//   return [left, top];
-// }
